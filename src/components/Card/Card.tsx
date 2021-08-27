5c4ce374370e16a1ff5035bf7e327ccc4b4ac850
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-08-27 18:21:49 +0800

    create Card for slider

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,25 @@
+import styles from "./styles.module.css";
+
+export const Card: React.FC = () => {
+  return (
+    <div className={styles.cardWrapper}>
+      {[...Array(3)].map((_, idx: number) => (
+        <div className={styles.card} key={idx}>
+          <div>
+            <img src="https://source.unsplash.com/random" alt={idx + "image"} />
+          </div>
+          <div>
+            <h3>Harry Styles</h3>
+            <h5>Web Developer</h5>
+          </div>
+          <div>
+            <h5>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              euismod ipsum et dui rhoncus auctor.
+            </h5>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
