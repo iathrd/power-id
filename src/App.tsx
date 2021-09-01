c424957365980b99e71d2e99d920bb5a7f857199
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-09-01 20:04:30 +0800

    created scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Home } from "./pages/Home/Home";
-import { Login } from "./pages/Login/Login";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { Login } from "./pages/Login/Login";
+
 import { Register } from "./pages/Register/Register";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 export const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Switch>
         <Route path="/" render={() => <Home />} exact />
         <Route path="/login" render={() => <Login />} />
